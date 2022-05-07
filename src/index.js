9d406ceffc6b96141a60422805a136ea4c0091ef
menglxa <menglxa@digitalchina.com>
2022-05-07 16:46:13 +0800

    eject

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client'
-import "./assets/base.css"
+import "./assets/base.less"
 import Router from './router'
 
 const root = createRoot(document.getElementById('root'))
