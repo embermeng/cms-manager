cca73c489ec3b5bef950d7010ff3f4aa72e03ae7
menglxa <menglxa@digitalchina.com>
2022-05-07 18:10:10 +0800

    页面布局完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Button } from 'antd';
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd';
+import logoImg from './assets/logo.png'
+
+const { Sider, Content } = Layout;
 
 export default function App() {
     return (
-        <div>
-            <Outlet></Outlet>
-        </div>
+        <Layout id='app'>
+            <header>
+                <img src={logoImg} alt="" className='logo' />
+                <div>右边</div>
+            </header>
+            <Layout>
+                <Sider>Sider</Sider>
+                <Content>
+                    <div>
+                        <Outlet />
+                    </div>
+                </Content>
+            </Layout>
+            <footer>Respect | Copyright &copy; 2022 Author 你单排吧</footer>
+        </Layout>
     )
 }
