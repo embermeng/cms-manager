081d7ab30c94354f7cb5d38f71449cd2410f97bd
menglxa <menglxa@digitalchina.com>
2022-05-16 15:47:21 +0800

    除了头像都完成了

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd';
 import Header from './components/Header';
@@ -6,6 +6,7 @@ import Aside from './components/Aside';
 import Bread from './components/Bread';
 
 export default function App() {
+    const [myKey, setMyKey] = useState(1)
     return (
         <Layout id='app'>
             <Header />
--- src/pages/Means.jsx
+++ src/pages/Means.jsx
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './less/Means.less'
-import { Form, Input, Button, message } from 'antd';
+import { Form, Input, Button, message, Upload } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import { UserInfoApi, UserInfoChgApi } from '../request/api';
 import { useNavigate } from 'react-router-dom';
 
-export default function Means() {
+// 将图片路径转base64
+function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+}
+
+// 限制图片大小，只能是200KB
+function beforeUpload(file) {
+	const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+	if (!isJpgOrPng) {
+		message.error('You can only upload JPG/PNG file!');
+	}
+	const isLt2M = file.size / 1024 / 1024 / 1024 < 200;
+	if (!isLt2M) {
+		message.error('请上传小于200KB的图!');
+	}
+	return isJpgOrPng && isLt2M;
+}
+
+export default function Means(props) {
 	const navigate = useNavigate()
+	const [loading, setLoading] = useState(false)
+	const [imageUrl, setImageUrl] = useState('')
 
 	useEffect(() => {
 		UserInfoApi().then((res) => {
@@ -39,6 +62,34 @@ export default function Means() {
 			message.error('用户名没改或没填写密码！')
 		}
 	}
+
+	// 点击了上传图片
+	const handleChange = info => {
+		if (info.file.status === 'uploading') {
+			setLoading(true)
+			return;
+		}
+		if (info.file.status === 'done') {
+			console.log(info);
+			// Get this url from response in real world.
+			getBase64(info.file.originFileObj, imageUrl => {
+				setLoading(false)
+				setImageUrl(imageUrl)
+				// 存储图片名称
+				localStorage.setItem('avatar', info.file.name)
+				// 触发Header组件更新
+				props.setMyKey(props.myKey++)
+			});
+		}
+	};
+
+	// 上传按钮
+	const uploadButton = (
+		<div>
+			{loading ? <LoadingOutlined /> : <PlusOutlined />}
+			<div style={{ marginTop: 8 }}>Upload</div>
+		</div>
+	);
 
 	return (
 		<div className='means'>
@@ -74,6 +125,19 @@ export default function Means() {
 					</Button>
 				</Form.Item>
 			</Form>
+			<p>点击下方修改头像：</p>
+			<Upload
+				name="avatar"
+				listType="picture-card"
+				className="avatar-uploader"
+				showUploadList={false}
+				action="/api/upload"
+				beforeUpload={beforeUpload}
+				onChange={handleChange}
+				headers={{'cms-token': localStorage.getItem('cms-token')}}
+			>
+				{imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+			</Upload>
 		</div>
 	)
 }
